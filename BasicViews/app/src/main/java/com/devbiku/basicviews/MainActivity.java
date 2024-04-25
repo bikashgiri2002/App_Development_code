eb58627dc5836bf8993489d0e44e12d8ca26c30b
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-04-26 05:22:17 +0530

    BasicView App making

---
+++ BasicViews/app/src/main/java/com/devbiku/basicviews/MainActivity.java
@@ -0,0 +1,109 @@
+package com.devbiku.basicviews;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button home,return_home;
+    TextView text;
+    String str=", Load jagannath will help you grow in your life if you work hard and respect everyone and help the needy";
+    String enter;
+    EditText name;
+    CheckBox success,hard_work,happiness,pain,discipline,peace;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        home=findViewById(R.id.btn_contact_us);
+        text=findViewById(R.id.txt_result);
+        name=findViewById(R.id.input_name);
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setHeight(1000);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enter=name.getText().toString();
+                text.setText(enter+str);
+            }
+        });
+        success=findViewById(R.id.checkbox_success);
+        hard_work=findViewById(R.id.checkbox_hard_work);
+        happiness=findViewById(R.id.checkbox_happiness);
+        pain=findViewById(R.id.checkbox_pain);
+        discipline=findViewById(R.id.checkbox_discipline);
+        peace=findViewById(R.id.checkbox_peace);
+        success.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (success.isChecked()){
+                    pain.setChecked(true);
+                    success.setChecked(false);
+                }
+            }
+        });
+        hard_work.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hard_work.isChecked()){
+                    success.setChecked(true);
+                    happiness.setChecked(true);
+                    pain.setChecked(false);
+                }
+            }
+        });
+        happiness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (happiness.isChecked()){
+                    pain.setChecked(true);
+                    happiness.setChecked(false);
+                }
+            }
+        });
+        pain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (pain.isChecked()){
+                    success.setChecked(false);
+                    happiness.setChecked(false);
+                    hard_work.setChecked(false);
+                    discipline.setChecked(false);
+                    peace.setChecked(false);
+                }
+            }
+        });
+        discipline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (discipline.isChecked()){
+                    happiness.setChecked(true);
+                    peace.setChecked(true);
+                    pain.setChecked(false);
+                }
+            }
+        });
+    }
+}
