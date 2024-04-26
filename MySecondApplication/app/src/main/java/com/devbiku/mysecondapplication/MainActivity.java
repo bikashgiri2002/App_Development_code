ad6c0b8a37196bf92418da4f2197595bfaaa725b
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-04-27 02:59:43 +0530

    toggle button and spinner

--- MySecondApplication/app/src/main/java/com/devbiku/mysecondapplication/MainActivity.java
+++ MySecondApplication/app/src/main/java/com/devbiku/mysecondapplication/MainActivity.java
@@ -3,9 +3,16 @@ package com.devbiku.mysecondapplication;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +23,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     TextView text,text2;
-    Button menu;
-    ConstraintLayout c;
+    Button ok;
+    RelativeLayout c;
     ImageView i;
+    RadioGroup group;
+    RadioButton green,blue,yellow;
+    ToggleButton showText;
+    ArrayAdapter spinnerAdapter;
+    Spinner countrySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +51,48 @@ public class MainActivity extends AppCompatActivity {
                 text.setBackgroundColor(Color.WHITE);
             }
         });
-        menu=findViewById(R.id.menu);
+        ok=findViewById(R.id.menu);
         text2=findViewById(R.id.textView2);
         c=findViewById(R.id.main);
-        menu.setOnClickListener(new View.OnClickListener() {
+        group=findViewById(R.id.radioGroupColour);
+        green=findViewById(R.id.radioButtonGreen);
+        blue=findViewById(R.id.radioButtonBlue);
+        yellow=findViewById(R.id.radioButtonYellow);
+        showText=findViewById(R.id.toggle_btn_text);
+        ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                c.setBackgroundColor(Color.BLACK);
                 text2.setVisibility(View.VISIBLE);
+                if (green.isChecked()){
+                    c.setBackgroundColor(Color.GREEN);
+                }
+                else if (blue.isChecked()){
+                    c.setBackgroundColor(Color.BLUE);
+                }
+                if (yellow.isChecked()){
+                    c.setBackgroundColor(Color.YELLOW);
+                }
             }
         });
-
+        showText.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    text.setTextColor(Color.BLACK);
+                    text.setBackgroundColor(Color.WHITE);
+                    c.setBackgroundColor(Color.BLACK);
+                }
+                else{
+                    text.setTextColor(Color.WHITE);
+                    text.setBackgroundColor(Color.BLACK);
+                    c.setBackgroundColor(Color.WHITE);
+                }
+            }
+        });
+        countrySpinner=findViewById(R.id.spinner_country);
+        spinnerAdapter=ArrayAdapter.createFromResource(this,R.array.country,android.R.layout.simple_spinner_item);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(spinnerAdapter);
 
     }
 }
