a3859e109ef1285bcb5097d35d8b7db59b8fe8bd
Bikash Kumar Giri <giribikashkumar097@gmail.com>
2024-05-17 17:55:30 +0530

    Stone paper scissor final code

--- StonePaperScissor/app/src/main/java/com/devbiku/stonepaperscissor/MainActivity.java
+++ StonePaperScissor/app/src/main/java/com/devbiku/stonepaperscissor/MainActivity.java
@@ -4,6 +4,8 @@ package com.devbiku.stonepaperscissor;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -19,11 +21,11 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ImageView user,computer;
-    RadioGroup userClick;
-    RadioButton stone, paper,scissor;
+
     int userScore=0,computerScore=0,userChoose,computerChoose;
-    TextView displayUserScore,displayComputerScore;
+    TextView displayUserScore,displayComputerScore,result;
     Random random = new Random();
+    Button refresh ;
 
 
     @Override
@@ -38,54 +40,83 @@ public class MainActivity extends AppCompatActivity {
         });
         user = findViewById(R.id.user_choose_img);
         computer = findViewById(R.id.computer_choose_img);
-        userClick = findViewById(R.id.userclicked);
-        stone = findViewById(R.id.stone_radio);
-        paper = findViewById(R.id.paper_radio);
-        scissor = findViewById(R.id.scissor_radio);
         displayUserScore =findViewById(R.id.txt_user_score);
         displayComputerScore =findViewById(R.id.txt_computer_score);
-        userClick.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.stone_radio){
-                    user.setImageResource(R.drawable.stone_final);
-                }else if (checkedId == R.id.paper_radio){
-                    user.setImageResource(R.drawable.paper_final);
-                }else{
-                    user.setImageResource(R.drawable.sciccors_final);
-                }
-            }
-        });
+        result = findViewById(R.id.result_txt);
+        refresh = findViewById(R.id.refresh_btn);
     }
-    public void clear(View view){
-        stone.setChecked(false);
-        paper.setChecked(false);
-        scissor.setChecked(false);
-    }
-    public void check(int computerChoose, int userChoose){
+
+    public void check(int userChoose,int computerChoose ){
         switch (userChoose){
             case 0:
+                user.setImageResource(R.drawable.stone_final);
                 if (computerChoose==1){
+                    computer.setImageResource(R.drawable.paper_final);
                     computerScore++;
+                    result.setText("Opps! You Lost");
                 } else if (computerChoose==2) {
+                    computer.setImageResource(R.drawable.sciccors_final);
                     userScore++;
+                    result.setText("Yups ! You Won");
+                }
+                else{
+                    computer.setImageResource(R.drawable.stone_final);
+                    result.setText("It's a Tie !");
                 }
                 break;
             case 1:
+                user.setImageResource(R.drawable.paper_final);
                 if (computerChoose==2) {
+                    computer.setImageResource(R.drawable.sciccors_final);
                     computerScore++;
+                    result.setText("Opps! you lost");
                 } else if (computerChoose==0) {
+                    computer.setImageResource(R.drawable.stone_final);
                     userScore++;
+                    result.setText("Yups ! You Won");
+                }
+                else{
+                    computer.setImageResource(R.drawable.paper_final);
+                    result.setText("It's a Tie !");
                 }
                 break;
             case 2:
+                user.setImageResource(R.drawable.sciccors_final);
                 if (computerChoose == 0){
+                    computer.setImageResource(R.drawable.stone_final);
                     computerScore++;
+                    result.setText("Opps! you lost");
                 } else if (computerChoose == 1) {
+                    computer.setImageResource(R.drawable.paper_final);
                     userScore++;
+                    result.setText("Yups ! You Won");
+                }
+                else{
+                    computer.setImageResource(R.drawable.sciccors_final);
+                    result.setText("It's a Tie !");
                 }
                 break;
         }
     }
+    private void changeScoreBoard(int userScore,int computerScore){
+        displayUserScore.setText(String.valueOf(userScore));
+        displayComputerScore.setText(String.valueOf(computerScore));
+    }
+    public void onClickOfImage(View v){
+        userChoose=Integer.parseInt(v.getTag().toString());
+        computerChoose=random.nextInt(3);
+        check(userChoose,computerChoose);
+        changeScoreBoard(userScore,computerScore);
+    }
+    public void onClickRefresh(View v){
+        userScore=0;
+        computerScore=0;
+        displayUserScore.setText(String.valueOf(userScore));
+        displayComputerScore.setText(String.valueOf(computerScore));
+        user.setImageResource(R.drawable.stone_final);
+        computer.setImageResource(R.drawable.stone_final);
+        result.setText("Result");
+    }
+
 
 }
